6200af3286f820ec0c2f4f0891095138d04fc966
Siphiwe-Manda <siphiwemanda@gmail.com>
2018-09-25 21:58:51 +0100

    Animation on the left menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-$('boxtext').addClass('animated slideInLeft');
+//$('boxtext').addClass('animated slideInLeft');
+
+window.addEventListener("load", startanimation);
+//const bigtext = document.getElementsByClassName('boxtext')
+//const par = document.getElementsByClassName('par')
+//const social = document.getElementsByClassName('social')
+
+
+
+function startanimation(){
+  var bigtext = document.getElementsByClassName('boxtext')
+  var par =document.getElementsByClassName('par')
+  var social =document.getElementsByClassName('social')
+
+  for(i=0;i <bigtext.length; i++) {
+  bigtext[i].classList.add("animated", "slideInLeft")
+}
+for(j=0;j <par.length; j++) {
+par[j].classList.add("animated", "slideInLeft")
+}
+for(k=0;k <social.length; k++) {
+social[k].classList.add("animated", "slideInLeft")
+}
+
+
+}
