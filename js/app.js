ca7f49d2f844630ee9ca64a4a38f0a74c0cd01a4
Siphiwe-Manda <siphiwemanda@gmail.com>
2018-09-26 15:37:16 +0100

    Cleaning the Javascript

--- js/app.js
+++ js/app.js
@@ -1,44 +1,58 @@
 //$('boxtext').addClass('animated slideInLeft');
 
-window.addEventListener("load", startanimation);
+//window.addEventListener("load", startanimation);
 //const bigtext = document.getElementsByClassName('boxtext')
 //const par = document.getElementsByClassName('par')
 //const social = document.getElementsByClassName('social')
 
 
 
-function startanimation(){
-  var bigtext = document.getElementsByClassName('boxtext')
-  var par =document.getElementsByClassName('par')
-  var social =document.getElementsByClassName('social')
 
-  for(i=0;i <bigtext.length; i++) {
-  bigtext[i].classList.add("animated", "slideInLeft")
-}
-for(j=0;j <par.length; j++) {
-par[j].classList.add("animated", "slideInLeft")
-}
-for(k=0;k <social.length; k++) {
-social[k].classList.add("animated", "slideInLeft")
-}
-
-
-}
 
 
 const model   ={
 
 }
 
-const octupus = {
+const octopus = {
+  init: function () {
+    leftside.init()
+
+  }
   //intiante the whole page
   //load the flex box but not the text
-  //load the annimation 
+  //load the annimation
 
 }
 
 
 const leftside = {
+
+  init: function(){
+    var bigtext = document.getElementsByClassName('boxtext')
+    var par =document.getElementsByClassName('par')
+    var social =document.getElementsByClassName('social')
+
+    for(i=0;i <bigtext.length; i++) {
+    bigtext[i].classList.add("animated", "slideInLeft")
+  }
+  for(j=0;j <par.length; j++) {
+  par[j].classList.add("animated", "slideInLeft")
+  }
+  for(k=0;k <social.length; k++) {
+  social[k].classList.add("animated", "slideInLeft")
+
+  social.addEventListener('mouseover', function () {
+    
+
+  })
+  }
+
+
+
+
+}
+
 
   //load all the CLasses
   //loop through all the classes
@@ -51,3 +65,11 @@ const leftside = {
 
 
 }
+
+const rightside= {
+  //add event listner to the the images
+  //when the mouse hovers on add backdrop
+}
+
+
+octopus.init();
